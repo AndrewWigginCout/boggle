3383adce9d012f4fed2a10835fc05ab91da96a36
AndrewWigginCout <x@aol.com>
2021-09-29 01:06:52 -0700

    stack

--- boggle.py
+++ boggle.py
@@ -4,4 +4,30 @@ def valid(s,b):#string, board
     m=len(b)
     n=len(b[0])
     stack=[]
-    for  
+    for x in range(n):
+        for y in range(m):
+            if b[y][x]==s[0]:
+                stack.append((y,x))
+
+    while stack:
+    y,x,cells=stack[-1]
+        for j in range(y-1,y+2):
+            for i in range(x-1,x+2):
+                if j<0: continue
+                if j>=m: continue
+                if i<0: continue
+                if i>=n: continue
+                if (j,i) in stack: continue
+                if b[j][i]==s[len(stack)]:
+                    stack.append((j,i,))
+        #failed pop stack
+        stack.pop()
+    #failed return False
+    return False
+
+
+
+#pseudocode
+#at every cell if the cell is the last letter of the word return True
+#              if the cell is not then continue to next cell
+#              if there are no more cells then pop this, then pop the path just checked
